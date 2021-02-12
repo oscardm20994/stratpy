9218d2c42f83fabf53194b3bfa9853ccbb51a28d
Oscar Dimdore-Miles <32623897+oscardm20994@users.noreply.github.com>
2021-02-12 15:28:51 +0000

    added docs

--- SSW/SSW.py
+++ SSW/SSW.py
@@ -1,8 +1,6 @@
 ##### script to define the SSW class developed by Oscar Dimdore-Miles
 ##### this class can contains data used to calcuate the state of the polar vortex and SSWs
-##### in a given dataset. This includes climatologies of various variables, wave driving, SSW time
-##### series, wavelet power spectra etc
-
+##### in a given dataset.
 
 #import necessary packages
 import iris
@@ -10,7 +8,6 @@ import numpy as np
 import iris.coord_categorisation as coord_cat
 from collections import Counter
 from stratpy.sudden_strat_warming.SSW_detector import SSW_counter
-
 
 ## build SSW class which has attributes for different SSW metrics
 class SSW:
@@ -23,8 +20,8 @@ class SSW:
 
 
 	#function to calculate when SSWs occur in the simulation/observations. Input is an iris cube contaning the daily
-	# ZMZW on the 10hPa level.
-	def get_SSW_dates(self, ZMZW, thresh = 0):
+	# ZMZW. Sends it off to the stratpy.processing.clean_cube function to constrain to the 10hPa level at 60N
+	def get_SSW_dates(self, ZMZW_cube, thresh = 0):
 
 		#call function to detect SSWs                
 		SSW_month, SSW_year, SSW_day = SSW_counter(ZMZW, self.years[0], self.years[-1], thresh)
@@ -32,9 +29,8 @@ class SSW:
 		#assign attributes of SSW instance to calulated SSW times 
 		self.SSW_month = SSW_month
 		self.SSW_year = SSW_year
-		self.SSW_day_number = SSW_year
+		self.SSW_day_number = SSW_day
 
-	
 
 	# function to add month, season and year metadata
 	def add_times(cube, time):
@@ -51,35 +47,21 @@ class SSW:
 		del cube.attributes['valid_max']
 		del cube.attributes['valid_min']
 	
-    ## define a loader function. this loads cubes of data. adds extra time coordinates
-	## using the user defined "add_times" function
-	def loader(fname):
-		##load all files defined in fname
-		cubes = iris.load(fname)
-		
-		#check how many cubes
-		if len(cubes) == 1:
-			cube = cubes[0]
-		
-		else:
-			cube = cubes
-		
-		return cube
-
 	
 	## function to give the SSW rate in a given dataset over the whole time period
 	def get_SSW_rate(self):
 		self.SSWrate = len(self.SSW_month)/np.float(len(self.years))
 		return
 	
-	### function to give the error bounds on a given SSW rate
+	### function to give the error bounds on a given SSW rate. takes as input the SSW tim
 	### using bootstrap resampling. gives the mean +- an estiamted error. 
-	def SSW_rate_Er(self, SSW_timeseries):
-		
+	def SSW_rate_Er(self, resample_interval):
+		#call SSW timeseries function         
+		get_SSWs_timeseries()
 		## resample SSW time series and calculate set of rates
 		rates = np.empty(0)
 		for i in range(10000):
-			rates = np.append(rates, np.mean(np.random.choice(SSW_timeseries, self.resample_interval, replace = False)))
+			rates = np.append(rates, np.mean(np.random.choice(SSW_timeseries, resample_interval, replace = False)))
 		
 		#get percentile of each of these rates		
 		percentiles = np.percentile(rates, np.arange(0,101,1))
@@ -96,7 +78,7 @@ class SSW:
 	##call above function and assign the errors in SSW rates to the instance. 
 	def get_SSW_rate_Er(self):
 	
-		self.SSWrate_Er = self.SSW_rate_Er(self.SSW_timeseries)[0]
+		self.SSWrate_Er = self.SSW_rate_Er(self, resample_interval)[0]
 		self.SSWrate_Er[0] = self.SSWrate_Er[0] - self.SSWrate
 		self.SSWrate_Er[1] = np.abs(self.SSWrate - self.SSWrate_Er[1])
 		return
---
+++ cube_process.py
@@ -0,0 +1,5 @@
+
+#script that performs pre-processing on iris cube objects pasrsed as arguments to
+#stratpy.<indexname> class functions. this includes cube constraining, sorting and adding
+# auxillary time coordinates etc 
+import iris
